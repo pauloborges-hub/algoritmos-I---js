7526e946268ab403ca4c1c08d3495427f0969a2c
PAULO BORGES <victorborges296@hotmail.com>
2023-10-22 13:24:04 -0300

    arquivos da aula 1

---
+++ aula1/listaLivros.js
@@ -0,0 +1,24 @@
+const precosLivros = [
+    {
+        titulo: "JavasCript",
+        preco: 25
+    },
+    {
+        titulo: "PHP",
+        preco: 15,
+    },
+    {
+        titulo: "Java",
+        preco: 30
+    },
+    {
+        titulo: "Elixir",
+        preco: 45,
+    },
+    {
+        titulo: "Go",
+        preco: 20
+    },
+];
+
+module.exports = precosLivros;
